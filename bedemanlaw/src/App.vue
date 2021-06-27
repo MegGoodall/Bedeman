516c8aa997ca8d4fa75185d9f5dba1dc92a974d2
MegGoodall <meg.goodall@gmail.com>
2021-06-27 20:37:46 +0200

    vue files created

---
+++ bedemanlaw/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <img alt="BA logo" src="./assets/BAlogo2.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+      
+  </div>
+</template>
+
+<script>
+//import Clients from './components/Client'
+
+export default {
+  name: 'app',
+  components: {
+    //Clients
+  }
+  /* data(){
+    return{
+      clients:[
+        {
+          firstName: "Bruno",
+          lastName: "Ross",
+          contact: "0984567645",
+          email: "bruno@gmail.com"
+        },
+
+        {
+          firstName: "Kate",
+          lastName: "Beckam",
+          contact: "0846567645",
+          email: "kate@gmail.com"
+        },
+
+        {
+          firstName: "Paul",
+          lastName: "Dylan",
+          contact: "0673457645",
+          email: "paul@gmail.com"
+        }
+
+
+      ]
+    } */
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ bedemanlaw/src/components/Clients.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>Clients<h1/>
+  </div>
+</template>
+
+<script>
+import Clients from '/.components/Clients';
+    export default {
+        name: 'Clients'
+        }
+
+</script>
+
+<style>
+
+</style>
